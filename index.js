c95ac97978dd404515cd6164c270fedc1a946d69
Dominic Tarr <dominic.tarr@gmail.com>
2017-05-11 22:00:02 +1200

    fall back to memory if there is no directory

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+'use strict'
 var json = require('./json')
 var Store = require('./store')
+var fs = require('fs')
+var path = require('path')
 
 module.exports = function (dir, codec) {
+  if(!dir)
+    return Store(
+      function (v, cb) { cb() },
+      function (k,v,cb) { cb() }
+    )
+
   codec = codec || json
+  var ready = false
+  function mkdir (cb) {
+    if(ready) cb()
+    else mkdirp(dir, function () {
+      ready = true
+      cb()
+    })
+  }
+
   return Store(function read (id, cb) {
     fs.readFile(path.join(dir, id), function (err, value) {
       if(err) return cb(err)
@@ -13,7 +31,18 @@ module.exports = function (dir, codec) {
   }, function write (id, value, cb) {
     try { value = codec.encode(value) }
     catch (err) { return cb(err) }
-    fs.writeFile(path.join(dir, id), value, cb)
+    mkdir(function () {
+      fs.writeFile(path.join(dir, id), value, cb)
+    })
   })
 }
+
+
+
+
+
+
+
+
+
 
